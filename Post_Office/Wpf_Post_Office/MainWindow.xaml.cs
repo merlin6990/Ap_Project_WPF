7f8810e57fab722946fa5fc1c19d86367f8515c3
unknown <aalimomen110@gmail.com>
2023-06-12 11:41:42 +0330

    Added 4 Windows + Designed the overall layout of Users Windows + Added a new project for logic and DB + initialized all the models of the program

--- Post_Office/Wpf_Post_Office/MainWindow.xaml.cs
+++ Post_Office/Wpf_Post_Office/MainWindow.xaml.cs
@@ -18,11 +18,78 @@ namespace Wpf_Post_Office
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Home_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Home_Panel.Visibility = Visibility.Visible;
+            Register_Customer_Panel.Visibility = Visibility.Collapsed;
+            Register_Order_Panel.Visibility = Visibility.Collapsed;
+            Order_List_Panel.Visibility = Visibility.Collapsed;
+            Order_Info_Panel.Visibility = Visibility.Collapsed;
+            Send_Email_Panel.Visibility = Visibility.Collapsed;
+        }
+
+        private void Register_Customer_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Home_Panel.Visibility = Visibility.Collapsed;
+            Register_Customer_Panel.Visibility = Visibility.Visible;
+            Register_Order_Panel.Visibility = Visibility.Collapsed;
+            Order_List_Panel.Visibility = Visibility.Collapsed;
+            Order_Info_Panel.Visibility = Visibility.Collapsed;
+            Send_Email_Panel.Visibility = Visibility.Collapsed;
+        }
+
+        private void Register_Order_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Home_Panel.Visibility = Visibility.Collapsed;
+            Register_Customer_Panel.Visibility = Visibility.Collapsed;
+            Register_Order_Panel.Visibility = Visibility.Visible;
+            Order_List_Panel.Visibility = Visibility.Collapsed;
+            Order_Info_Panel.Visibility = Visibility.Collapsed;
+            Send_Email_Panel.Visibility = Visibility.Collapsed;
+        }
+
+        private void Order_List_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Home_Panel.Visibility = Visibility.Collapsed;
+            Register_Customer_Panel.Visibility = Visibility.Collapsed;
+            Register_Order_Panel.Visibility = Visibility.Collapsed;
+            Order_List_Panel.Visibility = Visibility.Visible;
+            Order_Info_Panel.Visibility = Visibility.Collapsed;
+            Send_Email_Panel.Visibility = Visibility.Collapsed;
+        }
+
+        private void Order_Info_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Home_Panel.Visibility = Visibility.Collapsed;
+            Register_Customer_Panel.Visibility = Visibility.Collapsed;
+            Register_Order_Panel.Visibility = Visibility.Collapsed;
+            Order_List_Panel.Visibility = Visibility.Collapsed;
+            Order_Info_Panel.Visibility = Visibility.Visible;
+            Send_Email_Panel.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void Send_Email_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Home_Panel.Visibility = Visibility.Collapsed;
+            Register_Customer_Panel.Visibility = Visibility.Collapsed;
+            Register_Order_Panel.Visibility = Visibility.Collapsed;
+            Order_List_Panel.Visibility = Visibility.Collapsed;
+            Order_Info_Panel.Visibility = Visibility.Collapsed;
+            Send_Email_Panel.Visibility = Visibility.Visible;
+        }
+
+        private void Exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            //Going to The Log in Page NOT IMPLEMENTED YET
         }
     }
 }
